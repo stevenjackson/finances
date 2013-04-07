b2b901b59034806fcccde51f71746c9b00f86018
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-04-07 08:18:14 -0400

    Add rails for initial delivery

---
+++ delivery/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = 'c54b04f511481bc72f1917e776d192a969dbd62d614cef75625f3b63b53838111448b94afea96eadc30ff31ef7d03efb2eac62586afd8ac46e8f4ed99777d4dd'
