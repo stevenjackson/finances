b01e8adfb9c2f38e6245be93d2b059325c6a02e4
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-04-28 19:22:43 -0400

    Add fields to transaction table

--- gateway/data/sequel_migrations/003_create_transactions.rb
+++ gateway/data/sequel_migrations/003_create_transactions.rb
@@ -2,8 +2,14 @@ Sequel.migration do
   change do
     create_table :transactions do
       primary_key :id
+      FixNum :account_id
+      String :fit_id
       String :description
       FixNum :amount
+      FixNum :amount_in_pennies
+      String :nick_name
+      Time   :posted_at
+      String :type
     end
   end
 end
