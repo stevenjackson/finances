cdded56c225b855cc073650ab86073e5685d60ab
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-05-11 23:34:33 -0400

    Parse year properly from rails params

--- lib/finances/entities/date_params.rb
+++ lib/finances/entities/date_params.rb
@@ -2,18 +2,29 @@ module Finances::DateParams
 
   def parse_date(params)
     month = parse_month params[:month]
-    year = params[:year] || Time.new.to_date.year
+    year = parse_year params[:year]
     [month, year]
   end
 
-  def parse_month(month) 
+  def parse_month(month)
     case month
     when nil
-      Time.new.to_date.month
+      Date.today.month
     when String
       Date.strptime(month, "%b").month
     when Fixnum
       month
+    end
+  end
+
+  def parse_year(year)
+    case year
+    when nil
+      Date.today.year
+    when String
+      year.to_i
+    when Fixnum
+      year
     end
   end
 end
