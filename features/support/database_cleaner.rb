99c35c258995d0c46b5c99bb1581baee9c9d35a8
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-05-14 19:24:04 -0400

    Use FigNetwon for test database location

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -2,7 +2,7 @@ require 'sequel'
 require 'database_cleaner'
 require 'database_cleaner/cucumber'
 
-DB_URI = 'sqlite://gateway/data/test.sqlite'
+DB_URI = "sqlite://#{FigNewton.sqlite.relative_path}"
 DatabaseCleaner[:sequel, {:connection => Sequel.connect(DB_URI)}].strategy = :truncation
 
 Before do
