07f88a811b30040757c3d1b7cf6d508749660adf
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-10-13 08:50:30 -0400

    Update to latest version of listen gem

--- finances.gemspec
+++ finances.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "require_all"
   spec.add_dependency "ofx"
-  spec.add_dependency "listen", "~> 1.1.2"
+  spec.add_dependency "listen"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/finances/interactors/import_watcher.rb
+++ lib/finances/interactors/import_watcher.rb
@@ -1,22 +1,26 @@
 require 'listen'
 
 class Finances::ImportWatcher
+
+  FILTERS = [/\.csv$/, /\.ofx$/]
+
   def initialize(gateway)
     @gateway = gateway
   end
 
   def run(params={})
-    @listener = Listen.to(params[:import_path], relative_paths: false)
-      .filter(/\.csv$/, /\.ofx$/)
-      .ignore(%r{archive})
-      .change(&callback)
+    @listener = Listen.to(params[:import_path], ignore: %r{archive}, &callback)
 
       puts "Now watching #{params[:import_path]}"
     @listener.start
   end
 
   def stop
-    @listener.stop
+    begin
+      @listener.stop
+    rescue SystemExit
+      puts "Watcher stopping"
+    end
   end
 
   def start!
@@ -29,9 +33,26 @@ class Finances::ImportWatcher
   end
 
   def callback
+    @handler ||= default_callback
+    filterer
+  end
+
+  def default_callback
     Proc.new do |modified, added, removed |
       modified.each { |f| import f }
       added.each { |f| import f }
+    end
+  end
+
+  def filterer
+    Proc.new do |modified, added, removed|
+      @handler.call(filter(modified), filter(added), filter(removed))
+    end
+  end
+
+  def filter(files)
+    files.select do |file|
+      FILTERS.any? { |filter| filter =~ file } 
     end
   end
 
@@ -47,7 +68,7 @@ class Finances::ImportWatcher
   end
 
   def callback=(handler)
-    @listener.change &handler
+    @handler = handler
   end
 
 end
